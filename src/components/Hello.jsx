b8ebdcbc9e2dfa39695ff3c80ecc058066800688
blackart <blackart.ru@gmail.com>
2018-07-24 17:11:55 +0800

    init

---
+++ src/components/Hello.jsx
@@ -0,0 +1,21 @@
+"use strict";
+// src/components/Hello.tsx
+Object.defineProperty(exports, "__esModule", { value: true });
+var React = require("react");
+require("./Hello.css");
+function Hello(_a) {
+    var name = _a.name, _b = _a.enthusiasmLevel, enthusiasmLevel = _b === void 0 ? 1 : _b;
+    if (enthusiasmLevel <= 0) {
+        throw new Error('You could be a little more enthusiastic. :D');
+    }
+    return (<div className="hello">
+            <div className="greeting">
+                Hello {name + getExclamationMarks(enthusiasmLevel)}
+            </div>
+        </div>);
+}
+exports.default = Hello;
+// helpers
+function getExclamationMarks(numChars) {
+    return Array(numChars + 1).join('!');
+}
---
+++ src/index.tsx
@@ -0,0 +1,8 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import Hello from './components/Hello';
+
+ReactDOM.render(
+  <Hello name={'TypeScript'} enthusiasmLevel={3}/>,
+  document.getElementById('root') as HTMLElement
+);
